621c8af05dcd2b3abbd422e9e0bb42ad40d13a60
Velina Ivanova <velina.ivanova@alumni.ubc.ca>
2019-12-12 21:51:29 -0800

    Basic fourier transform graph for mic audio data

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+use rustfft::FFTplanner;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +12,15 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+pub fn apply_fft(data: &[f64]) -> Box<[f64]> {
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, fourier!");
+    let mut input: Vec<Complex<f64>> = data.iter().map(|&x| Complex::new(x,1.0)).collect();
+    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); 1024];
+
+    let mut planner = FFTplanner::new(false);
+    let fft = planner.plan_fft(1024);
+    fft.process(&mut input, &mut output);
+
+    let vector: Vec<f64> = output.iter().map(|&x| x.re).collect();
+    vector.into_boxed_slice()
 }
--- www/index.js
+++ www/index.js
@@ -1,1 +1,45 @@
 import * as fourier from "fourier";
+import * as d3 from "d3";
+
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let svg = d3.select("#graph")
+	.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+	.append("g");
+
+let x = d3.scaleLinear().domain([0, 512]).range([ 0, width ]);
+let y = d3.scaleLinear().domain([0, 300]).range([ height/2, 0 ]);
+
+const updateChart = data => {
+	d3.select("path").remove();
+	svg.append("path")
+		.datum(data)
+		.attr("fill", "none")
+		.attr("stroke", "white")
+		.attr("stroke-width", 2)
+		.attr("d", d3.line()
+			.x(function(_, i) { return x(i) })
+			.y(function(d) { return y(d) })
+		)
+};
+
+async function record() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+    const context = new AudioContext();
+    const source = context.createMediaStreamSource(stream);
+    const processor = context.createScriptProcessor(1024, 1, 1);
+    source.connect(processor);
+    processor.connect(context.destination);
+
+    processor.onaudioprocess = e => {
+		let data = fourier.apply_fft(e.inputBuffer.getChannelData(0));
+		// cut off the negative frequency part of the spectrum
+		updateChart(data.slice(0, 512));
+    };
+}
+
+record()
